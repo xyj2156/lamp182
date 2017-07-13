ad6e4acc4707e61505a65fdcef016757b17bd8d1
danxiaoteng <397177782@qq.com>
2017-07-14 03:32:35 +0800

    登录已完成

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -2,20 +2,75 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Http\Model\Admin\Member;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use Illuminate\Support\Facades\Crypt;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
+    /**
+     * 加载登录页面
+     */
     public function login()
     {
-        return view('home.login.login');
+        return view('home.login.login',['title' => '登录']);
     }
 
-    public function doligin()
+    /**
+     * 处理提交的数据
+     */
+    public function dologin(Request $request)
     {
-        echo 'sfda';
+        // 接收提交的信息
+        $data = $request -> only([
+            'email',
+            'password',
+            'code'
+        ]);
+        // 闪存数据
+        $request -> flash();
+
+        // 验证码是否正确
+        if(strtoupper($data['code']) !== session('code')){
+            return back() -> with('error','验证码错误');
+        }
+        // 验证邮箱或者手机号
+        if (!trim($data['email'])) {
+            return back() -> with('error','账号不能为空');
+        } else {
+            if (!preg_match('/^([0-9A-Za-z\\-_\\.]+)@([0-9a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?)$/i',$data['email']) && !preg_match('/^1[34578][0-9]{9}$/',$data['email'])){
+                return back() -> with('error','请输入正确的账号');
+            }
+        }
+        // 验证表单提交数据的规则
+        $this->validate($request, [
+            'password'=>'required|between:6,18'
+        ],[
+            'password.required'=>'必须输入密码',
+            'password.between'=>'密码长度必须在6-18位之间'
+        ]);
+
+        // 根据表单name值 email 来查出这条数据
+        $user = Member::where('email',$data['email']) -> orwhere('phone',$data['email']) -> select('email','phone','password') -> first();
+        // 验证用户
+        if (!$user) {
+            return back() -> with('error','没有这个用户名');
+        } else {
+            // 验证密码是否正确
+            if (Crypt::decrypt($user['password']) !== $data['password']) {
+                return back() -> with('error','账号或密码错误');
+            };
+        }
+
+        // 获取到当前登录成功的时间
+        $user -> ltime = time();
+        // 执行跟新数据
+        $user  -> save();
+
+        // 将用户信息添加到session中
+        session(['home_user' => $user]);
+        return redirect('/');
     }
 }
--- resources/views/home/login/login.blade.php
+++ resources/views/home/login/login.blade.php
@@ -3,12 +3,19 @@
 @section('style')
     <link href="{{asset('home/css/login.css')}}" rel="stylesheet" type="text/css" media="all"/>
 @endsection
+@section('script')
+    <script>
+        $('img[alt]').click(function () {
+            this.src = '{{url('code')}}/' + Math.random().toString().replace('.', '') + '.jpg';
+        });
+    </script>
+@endsection
 
 @section('content')
 
                 <div class="content_top">
                     <div class="back-links">
-                        <p><a href="index.html">Home</a> &gt;&gt;&gt;&gt; <a href="#" class="active">Contact</a></p>
+                        <p><a href="{{url('/')}}">主页</a> &gt;&gt;&gt;&gt; <a href="#" class="active">登录</a></p>
                     </div>
                     <div class="clear"></div>
                 </div>
@@ -23,7 +30,7 @@
                                     <tr>
                                         <th>账户</th>
                                         <td width="245">
-                                            <input id="email" type="text" name="email" placeholder="电子邮箱/手机号" autocomplete="off" value=""></td>
+                                            <input id="email" type="text" name="email" placeholder="电子邮箱/手机号" autocomplete="off" value="{{old('email')}}"></td>
                                         <td>
                                         </td>
                                     </tr>
@@ -35,11 +42,14 @@
                                         <td>
                                         </td>
                                     </tr>
-                                    <tr id="tr-vcode" style="display:none;">
+                                    <tr id="tr-vcode" >
                                         <th>验证码</th>
                                         <td width="245">
                                             <div class="valid">
-                                                <input type="text" name="vcode"><img class="vcode" src="passport/vcode?_=1411476793" width="85" height="35" alt="">
+                                                <input type="text" style='width:190px' class="tpl-form-input" required id="user-name" placeholder="请输入验证码" name="code">
+                                                <div>
+                                                    <img src="{{ url('code') }}/{{rand(10000, 99999)}}.jpg" alt="验证码" style='margin-left: 200px;margin-top:-35px;'>
+                                                </div>
                                             </div>
                                         </td>
                                         <td>
@@ -62,16 +72,22 @@
                                     </tr>
                                     </tbody>
                                 </table>
-                                <input type="hidden" name="refer" value="site/">
                             </form>
                         </div>
                     </div>
                     <div class="col span_1_of_3">
                         <div class="reg">
                             <p>还没有账号？<br>赶快免费注册一个吧！</p>
-                            <a class="reg-btn" href="#">立即免费注册</a>
+                            <a class="reg-btn" href="{{url('/reg')}}">立即免费注册</a>
                         </div>
                     </div>
                 </div>
 
 @endsection
+
+
+<script>
+    $('img[alt]').click(function () {
+        this.src = '{{url('code')}}/' + Math.random().toString().replace('.', '') + '.jpg';
+    });
+</script>
