74f185799de8b728625677a7afa52306ddc82b72
从没开始 <506907958@qq.com>
2017-07-13 21:21:58 +0800

    和杨鹏亮合体添加他写的影厅播放信息,与我写的影厅硬件信息合并

--- app/Http/Model/Admin/FilmPlay.php
+++ app/Http/Model/Admin/FilmPlay.php
@@ -4,12 +4,15 @@ namespace App\Http\Model\Admin;
 
 use Illuminate\Database\Eloquent\Model;
 
-class FilmRoom extends Model
+class FilmPlay extends Model
 {
     //关联的表
-    protected $table = 'film_rooms';
+    protected $table = 'film_plays';
     //主键
     protected $primaryKey = 'id';
     //黑名单
     protected $guarded = [];
+
+    //是否自动维护 create_at 和 update_at
+    public $timestamps = false;
 }
--- resources/views/admin/film_room/index.blade.php
+++ resources/views/admin/film_room/index.blade.php
@@ -5,16 +5,10 @@
     <div class="container-fluid am-cf">
         <div class="row">
             <div class="am-u-sm-12 am-u-md-12 am-u-lg-9">
-                <div class="page-header-heading"><span class="am-icon-home page-header-heading-icon"></span> 用户管理 <small>江洋八子</small></div>
+                <div class="page-header-heading"><span class="am-icon-home page-header-heading-icon"></span> 影厅播放管理 <small>江洋八子</small></div>
                 <p class="page-header-description">简单组合。。。干不简单的事情。。。</p>
             </div>
-            {{--<!-- 搜索 -->--}}
-            <div class="am-fl tpl-header-search">
-                <form class="tpl-header-search-form" action="{{url('admin/user')}}">
-                    <button class="tpl-header-search-btn am-icon-search" type="submit"></button>
-                    <input class="tpl-header-search-box" name="search" type="text" value="{{$search['search'] or ''}}" placeholder="搜索用户名。.">
-                </form>
-            </div>
+
         </div>
     </div>
     <div class="row-content am-cf">
@@ -22,36 +16,45 @@
         <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
             <div class="widget am-cf">
                 <div class="widget-head am-cf">
-                    <div class="widget-title am-fl">用户列表</div>
+                    <div class="widget-title am-fl">播放列表</div>
                 </div>
                 <div class="widget-body  widget-body-lg am-fr">
                     <table width="100%" class="am-table am-table-compact am-table-striped tpl-table-black " id="example-r">
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>用户名</th>
-                                <th>最后登陆</th>
-                                <th>注册IP</th>
-                                <th>phone</th>
-                                <th>email</th>
+                                <th>影厅名</th>
+                                <th>影片</th>
+                                <th>开播时间</th>
+                                <th>结束时间</th>
+                                <th>状态</th>
                                 <th>操作</th>
                             </tr>
                         </thead>
                         <tbody>
-                        @foreach($data as $k => $v)
+                        @foreach ($data as $kk => $vv)
                             <tr class="gradeX">
-                                <td>{{$v -> id}}</td>
-                                <td><a href="{{url('admin/user/'.$v -> id)}}">{{$v['username']}}</a></td>
-                                <td>{{date('Y-m-d H:i:s', $v['ltime'])}}</td>
-                                <td>{{$v['ip']}}</td>
-                                <td>{{$v['phone']}}</td>
-                                <td>{{$v['email']}}</td>
+                                <td>{{$vv['id']}}</td>
+                                <td><a href="">{{$ridsR[$vv -> rid]}}</a></td>
+                                <td><a href="{{url('admin/'.$vv['fid'])}}">{{$fidsR[$vv -> fid]}}</a></td>
+                                <td>{{date('Y-m-d H:i:s', $vv['start_time'])}}</td>
+                                <td>{{date('Y-m-d H:i:s', $vv['end_time'])}}</td>
+                                {{--状态--}}
+                                @if($time < $vv['start_time'])
+                                    <td>该电影在{{date('Y-m-d H:i:s', $vv['start_time'])}}播放,请等待..</td>
+                                @else
+                                    @if($time > $vv['start_time'] && $time < $vv['end_time'])
+                                        <td><span style="color:green">{{$ridsR[$vv -> rid]}}</span>：　<span style="color:orange">{{$fidsR[$vv -> fid]}}</span>　正在播放..</td>
+                                    @elseif($time > $vv['end_time'])
+                                        <td>该电影已播放完毕..</td>
+                                    @endif
+                                @endif
                                 <td>
                                     <div class="tpl-table-black-operation">
-                                        <a href="{{url("admin/user/{$v['id']}/edit")}}" title="编辑">
+                                        <a href="{{url('admin/filmroom')}}/{{$vv->id}}/edit" title="编辑">
                                             <i class="am-icon-pencil"></i> 编辑
                                         </a>
-                                        <a href="javascript:;" onclick="member_delete({{$v['id']}})" class="tpl-table-black-operation-del" title="删除">
+                                        <a href="javascript:;" onclick="member_delete({{$vv['id']}})" class="tpl-table-black-operation-del" title="删除">
                                             <i class="am-icon-trash"></i> 删除
                                         </a>
                                     </div>
@@ -71,7 +74,7 @@
                 btn: ['忍痛删除','舍不得'] //按钮
             }, function(){
                 $.post(
-                    '{{url('admin/user')}}/'+id,
+                    '{{url('admin/filmroom')}}/'+id,
                     {
                         '_method' : 'delete',
                         '_token' : '{{csrf_token()}}'
@@ -83,7 +86,7 @@
                         } else {
                             icon = 6;
                             setTimeout(function(){
-                                location.href = '/admin/user';
+                                location.href = '/admin/filmroom';
                             },1000);
                         }
                         layer.alert(msg.msg,{icon:icon});
