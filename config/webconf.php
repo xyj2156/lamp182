67a88b00d20d8084d4a1cb17120cb707195b9530
从没开始 <506907958@qq.com>
2017-07-19 23:46:56 +0800

    东西别乱动

--- config/webconf.php
+++ config/webconf.php
@@ -4,9 +4,9 @@
   'tel' => '18888888888',
   'email' => '506907958@qq.com',
   'logo' => '/uploads/logo_thumb/2017-07-15-17-38-15-5969e2878b40b.png',
-  'count' => '<script></script>',
+  'count' => '<script>layer.msg("测试统计脚本")</script>',
   'icp' => '鲁ICP备11016544号',
   'copyright' => 'CopyRight © xxxx oooo公司 保留所有权利。',
   'keywords' => '天气,电影,卖票,便捷卖票',
-  'description' => '开发过程异常艰难,我是组长,可能是太严苛了.因为我把对我自己的要求加到其他人身上,无语......',
+  'description' => '测试描述',
 );
