e9b16e474c66240acfe8814a37a2631f932c092f
www <www@iZ2zei94exex0qft69wwu8Z.(none)>
2017-09-14 16:48:32 +0800

    打算把 sql 放上去

--- config/webconf.php
+++ config/webconf.php
@@ -4,7 +4,7 @@
   'tel' => '18888888888',
   'email' => '506907958@qq.com',
   'logo' => '/uploads/logo_thumb/2017-07-15-17-38-15-5969e2878b40b.png',
-  'count' => '<script>layer.msg("测试统计脚本")</script>',
+  'count' => '<script>layer.msg("测试统计脚本...")</script>',
   'icp' => '鲁ICP备11016544号',
   'copyright' => 'CopyRight © xxxx oooo公司 保留所有权利。',
   'keywords' => '天气,电影,卖票,便捷卖票',
